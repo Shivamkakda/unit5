f856a6ae2ca3d8cfce1e68116f78eec1a1b8e12e
Shivam Sharma <shivampandit635@gmail.com>
2021-12-23 14:51:07 +0530

    webpack

---
+++ Webpack/src/component.js
@@ -0,0 +1,6 @@
+const Data = `
+<img id="logo" alt="">
+<div class="container">
+</div>
+`
+export default Data
---
+++ Webpack/src/index.js
@@ -0,0 +1,27 @@
+import "./index.css";
+
+import Logo from "./images/ss.jpg"
+
+import Data from "./component.js";
+function add(a,b){
+    return a+b;
+}
+console.log(add(3,4))
+
+let getRoot = document.querySelector("#root");
+getRoot.innerHTML = Data;
+
+
+let logo = document.getElementById("logo");
+logo.src = Logo;
+logo.classList.add("imgs")
+
+const h1 = document.createElement("h1");
+h1.innerText = "Hii My Name is Shivam Sharma";
+
+h1.classList.add("text1");
+const p = document.createElement("p");
+p.innerText = "My name is Shivam Shrama, I am From Ghaziabad Uttar Pardesh. I have done MCA From RD Engineering college Duhai. Now I am persuing Full Stack Web Devlopment Course from Masai School"
+p.classList.add("text2")
+getRoot.append(logo,h1,p)
+
